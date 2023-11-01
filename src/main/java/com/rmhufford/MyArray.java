5168dd8c5a53bdc6d51dba1dd7cbe11910af4660
Ryan Hufford <ryan@rmhufford.com>
2023-11-01 16:00:45 -0400

    start array

---
+++ src/main/java/com/rmhufford/MyArray.java
@@ -0,0 +1,17 @@
+package com.rmhufford;
+
+public class MyArray {
+  private int[] array;
+  private int size;
+
+  public MyArray(int capacity) {
+    this.array = new int[capacity];
+    this.size = 0;
+  }
+
+  //get
+
+  //set
+
+  //insert
+}
